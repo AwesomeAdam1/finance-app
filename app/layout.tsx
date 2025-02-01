d7b8accd9d70ef167b9eb6036801ce51a68699e7
AwesomeAdam1 <adamwang.aw.2006@gmail.com>
2025-02-01 18:22:05 -0500

    Added Setup/Auth/Hono.js/Header/Drizzle and Neon/Get and Post APIs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { Toaster } from "@/components/ui/sonner"
+import { QueryProvider } from "@/providers/query-providers";
+import { SheetProvider } from "@/providers/sheet-provider";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,12 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <QueryProvider>
+            <SheetProvider />
+            <Toaster />
+            {children}
+          </QueryProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
